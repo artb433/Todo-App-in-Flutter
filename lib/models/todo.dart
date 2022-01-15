02e491ce08d8b9258c7b03d03a22a190af6a6b66
Art b <94374097+artb433@users.noreply.github.com>
2022-01-15 20:29:27 +0000

    my

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,50 +1,50 @@
-// To parse this JSON data, do
-//
-//     final todo = todoFromJson(jsonString);
+// // To parse this JSON data, do
+// //
+// //     final todo = todoFromJson(jsonString);
 
-import 'dart:convert';
+// import 'dart:convert';
 
-List<Todo> todoFromJson(String str) =>
-    List<Todo>.from(json.decode(str).map((x) => Todo.fromJson(x)));
+// List<Todo> todoFromJson(String str) =>
+//     List<Todo>.from(json.decode(str).map((x) => Todo.fromJson(x)));
 
-String todoToJson(List<Todo> data) =>
-    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+// String todoToJson(List<Todo> data) =>
+//     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-class Todo {
-  Todo({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.isCompleted,
-    required this.deadline,
-    required this.v,
-  });
+// class Todo {
+//   Todo({
+//     required this.id,
+//     required this.title,
+//     required this.description,
+//     required this.isCompleted,
+//     required this.deadline,
+//     required this.v,
+//   });
 
-  String id;
-  String title;
-  String description;
-  bool isCompleted;
-  DateTime deadline;
-  int v;
+//   String id;
+//   String title;
+//   String description;
+//   bool isCompleted;
+//   DateTime deadline;
+//   int v;
 
-  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
-        id: json["_id"],
-        title: json["title"],
-        description: json["description"],
-        isCompleted: json["isCompleted"],
-        deadline: DateTime.parse(json["deadline"]),
-        v: json["__v"],
-      );
+//   factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+//         id: json["_id"],
+//         title: json["title"],
+//         description: json["description"],
+//         isCompleted: json["isCompleted"],
+//         deadline: DateTime.parse(json["deadline"]),
+//         v: json["__v"],
+//       );
 
-  Map<String, dynamic> toJson() => {
-        "_id": id,
-        "title": title,
-        "description": description,
-        "isCompleted": isCompleted,
-        "deadline": deadline.toIso8601String(),
-        "__v": v,
-      };
-}
+//   Map<String, dynamic> toJson() => {
+//         "_id": id,
+//         "title": title,
+//         "description": description,
+//         "isCompleted": isCompleted,
+//         "deadline": deadline.toIso8601String(),
+//         "__v": v,
+//       };
+// }
 
 
 
